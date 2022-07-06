d1a8776d06497c77a44f5b5ece3fe319797d2fc3
= <diegoalt.g@hotmail.com>
2022-07-06 12:00:07 -0400

    Se añade codigo en los archivos

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 window.confirm("Bienvenido");
 
 const btnInfo = document.getElementById("btnInfo");
+const btnAceptar = document.querySelectorAll(".btnAceptar")
+const btnEliminar = document.querySelectorAll(".btnEliminar")
 
 cargarEventos();
 function cargarEventos(){
     btnInfo.addEventListener("click",()=>{
-        alert("Hello Again")
+        alert("Información....")
+    })
+    
+    btnAceptar.forEach(elemento=>{
+        console.log(elemento);
+        elemento.addEventListener("click",e=>{
+            alert("Aceptando...")
+            console.log(e.target)
+        })
+
+    })
+
+    btnEliminar.forEach(elemento=>{
+        console.log(elemento);
+        elemento.addEventListener("click",e=>{
+            alert("Eliminando...")
+            console.log(e.target)
+        })
+
     })
 }
+
